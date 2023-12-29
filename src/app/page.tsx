001ec4cdaedd3aaf170b7b7fbb2acd003b2af437
Dominik Reinert <github.unlikable069@passmail.net>
2023-12-29 10:26:35 +0100

    make validatio nwork

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,24 +14,40 @@ const schema = z.object({
   address: z.string().min(2, 'min 2'),
   postcode: z.string().min(2, 'min 2'),
   country: z.string().min(1, 'min 1'),
-  email: z.coerce.string().email().min(5, 'min 5'),
-  website: z.coerce.string().optional(),
+  taxNumber: z.string().min(2),
+  email: z.string().email().optional(),
+  website: z.string().optional(),
   bankName: z.string().optional(),
   iban: z.string().optional(),
   bic: z.string().optional(),
-  taxNumber: z.string().min(2)
 })
 
 
 
 export default function Home() {
+  const onSubmit = React.useCallback((values: z.infer<typeof schema>) => {
+    alert(JSON.stringify(values));
+  }, [])
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Card>
+        <CardHeader><CardTitle>Rechnung erstellen</CardTitle></CardHeader>
+        <CardContent>
+          <Step1 onSubmit={onSubmit} />
+        </CardContent>
+      </Card>
+    </main>
+  )
+}
+
+
+function Step1({ onSubmit }: { onSubmit: (values: z.infer<typeof schema>) => void }): JSX.Element {
   const form = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
     defaultValues: {
       name: "",
       address: "",
       country: "",
-      email: "",
       postcode: "",
       website: "",
       bankName: "",
@@ -42,159 +58,148 @@ export default function Home() {
   })
 
   const { isDirty, isValid } = form.formState;
-  const onSubmit = React.useCallback((values: z.infer<typeof schema>) => {
-    alert(JSON.stringify(values));
-  }, [])
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Card>
-        <CardHeader><CardTitle>Rechnung erstellen</CardTitle></CardHeader>
-        <CardContent>
-          <Form {...form}  >
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-              <div className='flex gap-4'>
-                <div className='flex gap-4 flex-col'>
-                  <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Dein Name</FormLabel>
-                        <FormControl>
-                          <Input required placeholder="Max Mustermann" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="address"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Straße + Hausnummer</FormLabel>
-                        <FormControl>
-                          <Input required placeholder="Musterstraße 4" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="postcode"
-                    render={({ field }) => (
-                      <FormItem >
-                        <FormLabel>PLZ + Ort</FormLabel>
-                        <FormControl>
-                          <Input required placeholder="11111 Musterstadt" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="country"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Land</FormLabel>
-                        <FormControl>
-                          <Input required placeholder="Deutschland" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Email</FormLabel>
-                        <FormControl>
-                          <Input placeholder="max@mustermann.de" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="website"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Webseite</FormLabel>
-                        <FormControl>
-                          <Input placeholder="mustermann.de" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <div className='flex gap-4 flex-col'>
-                  <FormField
-                    control={form.control}
-                    name="bankName"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Bankname</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Max Mustermann" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="iban"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>IBAN</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Musterstraße 4" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="bic"
-                    render={({ field }) => (
-                      <FormItem >
-                        <FormLabel>BIC</FormLabel>
-                        <FormControl>
-                          <Input placeholder="11111 Musterstadt" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="taxNumber"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Steuernummer</FormLabel>
-                        <FormControl>
-                          <Input required {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-              </div>
+  console.log(isDirty, isValid)
+  return <Form {...form}  >
+    <form onSubmit={form.handleSubmit(onSubmit)}>
+      <div className='flex gap-4'>
+        <div className='flex gap-4 flex-col'>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Dein Name</FormLabel>
+                <FormControl>
+                  <Input required placeholder="Max Mustermann" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Straße + Hausnummer</FormLabel>
+                <FormControl>
+                  <Input required placeholder="Musterstraße 4" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="postcode"
+            render={({ field }) => (
+              <FormItem >
+                <FormLabel>PLZ + Ort</FormLabel>
+                <FormControl>
+                  <Input required placeholder="11111 Musterstadt" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="country"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Land</FormLabel>
+                <FormControl>
+                  <Input required placeholder="Deutschland" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="max@mustermann.de" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="website"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Webseite</FormLabel>
+                <FormControl>
+                  <Input placeholder="mustermann.de" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className='flex gap-4 flex-col'>
+          <FormField
+            control={form.control}
+            name="bankName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Bankname</FormLabel>
+                <FormControl>
+                  <Input placeholder="Max Mustermann" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="iban"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>IBAN</FormLabel>
+                <FormControl>
+                  <Input placeholder="Musterstraße 4" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="bic"
+            render={({ field }) => (
+              <FormItem >
+                <FormLabel>BIC</FormLabel>
+                <FormControl>
+                  <Input placeholder="11111 Musterstadt" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="taxNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Steuernummer</FormLabel>
+                <FormControl>
+                  <Input required {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </div>
 
-              <div className='mt-4 flex justify-end'>
-                <Button disabled={!isValid || !isDirty} type='submit'>Submit</Button></div>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
-    </main>
-  )
+      <div className='mt-4 flex justify-end'>
+        <Button disabled={!isValid || !isDirty} type='submit'>Next</Button></div>
+    </form>
+  </Form>
 }
