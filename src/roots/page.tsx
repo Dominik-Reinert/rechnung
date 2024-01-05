ff86bc38d759b0ad905e59028f1b966aa5e45d88
Dominik Reinert <github.unlikable069@passmail.net>
2024-01-05 17:27:50 +0100

    main: add some more work

--- src/roots/page.tsx
+++ src/roots/page.tsx
@@ -1,7 +1,22 @@
-import { CreationWizard } from "@/components/creation/wizard";
+import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
-  return <CreationWizard />
+  const t = useTranslations('home');
+  return <main className="flex flex-col justify-between h-[99svh]">
+    <header className="flex flex-row w-full justify-between p-6 text-gray-900 font-bold">
+      <span>Finya</span>
+      <div>
+        <Button>{t('createButtonLabel')}</Button>
+      </div>
+    </header>
+    <section className="p-6">
+      <h1>{t("title")}</h1>
+      <h2>{t("subTitle")}</h2>
+      <div className="flex justify-end"><Button>{t('createButtonLabel')}</Button></div>
+    </section>
+    <footer>footer</footer>
+  </main>
 }
 
 
