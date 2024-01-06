96fafc6d0d4d9860eb10b485e76f2b4c0415d09c
Dominik Reinert <github.unlikable069@passmail.net>
2024-01-06 07:49:46 +0100

    main: add router stuff and base page

--- src/roots/page.tsx
+++ src/roots/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { router } from "@/lib/roots-router";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
     <header className="flex flex-row w-full justify-between p-6 text-gray-900 font-bold align-baseline">
       <span className="text-xl md:text-2xl leading-normal">Finya</span>
       <div>
-        <Button>{t('createButtonLabel')}</Button>
+        <Button onClick={() => router.}>{t('createButtonLabel')}</Button>
       </div>
     </header>
     <section className="p-6">
